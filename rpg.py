b1297efdbaec5deeb03937de205399b5093e0941
Gladwingeorge <76112629+Gladwingeorge@users.noreply.github.com>
2021-02-21 13:44:50 +0530

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -30,10 +30,7 @@ pass_str = StringVar()
 
 def Generator():
     password = ''
-    for x in range(0, 4):
-        password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(
-            string.digits) + random.choice(string.punctuation)
-    for y in range(pass_len.get() - 4):
+    for x in range(pass_len.get()):
         password = password + random.choice(
             string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
     pass_str.set(password)
